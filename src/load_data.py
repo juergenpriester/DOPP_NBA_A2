42aab5cf41114722940ab52b597e378b421de9a3
filip2faber <filip2faber@gmail.com>
2025-01-11 13:27:55 +0100

    inital commit

--- src/load_data.py
+++ src/load_data.py
@@ -3,30 +3,33 @@ import pandas as pd
 from nba_api.stats.endpoints import TeamGameLogs
 
 from utils import check_create_dir
-from constants import DATA_DIR
+from constants import DATA_DIR, SEASONS
 
 
 
-def load_from_api():
-    gamedatapull = TeamGameLogs(
-            league_id_nullable ='00', # nba 00, g_league 20, wnba 10
-            team_id_nullable = '', # can specify a specific team_id
-#            season_nullable = '2023-24',
-            season_type_nullable = 'Regular Season' # Regular Season, Playoffs, Pre Season
-        )
-        
-    df_season = gamedatapull.get_data_frames()[0]     
-    print(df_season.head())
-    return df_season
+def load_from_api(seasons: list[str]) -> pd.DataFrame:
+    df = pd.DataFrame()
+    for season in seasons:
+        gamedatapull = TeamGameLogs(
+                league_id_nullable ='00', # nba 00, g_league 20, wnba 10
+                season_type_nullable = 'Regular Season', # Regular Season, Playoffs, Pre Season
+                season_nullable = season
+            )
+            
+        df_season = gamedatapull.get_data_frames()[0]    
+        df = pd.concat([df, df_season]) 
+    return df
+
+
+def load_from_csv(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    return df
 
 
 def main():
-    # check_create_dir('data')
     check_create_dir(DATA_DIR)
-    api_data = load_from_api()
+    api_data = load_from_api(SEASONS)
     api_data.to_csv(DATA_DIR + 'nba_data.csv', index=False)
-
-
 
 
 if __name__ == '__main__':
