5e323ef8d1dae065c500cbcc51de1ea985117642
filip2faber <filip2faber@gmail.com>
2025-01-25 16:53:26 +0100

    Added dropping player names

--- src/preprocess.py
+++ src/preprocess.py
@@ -233,6 +233,8 @@ def merge_games_injuries():
     # df_team = df_team.explode('INJURED_PLAYERS_AWAY')
     df_team_better['INJURED_PLAYERS_AWAY_STATS'] = df_team_better.apply(get_player_stats_better, axis=1, player_col='INJURED_PLAYERS_AWAY')
 
+    df_team_better = df_team_better.drop(columns=['INJURED_PLAYERS_HOME', 'INJURED_PLAYERS_AWAY'], inplace=False)
+
     df_team_better.to_csv(os.path.join(TEAMLOG_DATA, 'team_data_combined_injuries_explode_better.csv'), index=True)
 
 
